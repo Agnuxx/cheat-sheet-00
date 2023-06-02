02311dd1030aefb3a48d0948b2e1939a091c1e4d
Agnuxx <agnusvictorveras@gmail.com>
2023-06-02 12:02:03 -0300

    first commit

---
+++ src/Routes/index.js
@@ -0,0 +1,21 @@
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import Welcome from '../pages/Welcome';
+import SignIn from '../pages/SignIn';
+
+const Stack = createNativeStackNavigator();
+
+export default function Routes() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen 
+      name="Welcome" 
+      component={Welcome} />
+      
+      <Stack.Screen 
+      name="SignIn" 
+      component={SignIn} />
+    
+    </Stack.Navigator>
+  );
+}
---
+++ src/pages/Welcome/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {View} from 'react-native';
+
+export default function Bem_Vindo(){
+    return (
+        <View>
+            <Text>Tela Bem-Vindo</Text>
+        </View>
+    );
+}
